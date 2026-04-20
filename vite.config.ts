db70aeb7476fe789545959a3969539c188480cb8
RajarshiPenti <rajarshipenti2000@gmail.com>
2026-04-20 23:04:08 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: 'localhost'
   },
-  base: '/Dwarakas-Restaurent/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 900
   }
