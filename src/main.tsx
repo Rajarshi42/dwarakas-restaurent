001f0d298164a3ea89e688806b10e10965d2b6e5
RajarshiPenti <Rajarshi.Penti@soulax.io>
2026-04-24 17:10:12 +0530

    fix routing for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-
+import { HashRouter } from 'react-router-dom';
 // Disable browser scroll restoration before anything renders
 if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
 window.scrollTo(0, 0);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/dwarakas-restaurent">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
